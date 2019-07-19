c8d4d91bdd9279a6d4a059466f76f8c210bdf6ca
AlOrozco53 <alorozco.patriot53@gmail.com>
2019-07-19 16:36:35 -0500

    [ADDED] support to parse base64 encoded images

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+# Detección de ID
+
+El módulo en `node.js` que hace la detección de ID se encuentra en [`lineDetection.js`](/src/services/lineDetection.js).
+
+La función que hace todo el trabajo es `boxDoc(img, areaLoc, coverPercentage)` donde `areaLoc` son las coordenadas del área donde hay que localizar
+la ID. `coverPercentage` es un número entre 0 y 1 que indica qué porcentaje del área debe cubrir la ID a detectar; `areaLoc` es un objeto de la forma:
+
+```json
+{
+	topX: 10.3,
+	topY: 13.4,
+	bottomX: 56.78,
+	bottomY: 45.93
+}
+```
+
+donde `topX` y `topY` representan la coordenada superior izquierda del rectángulo y
+`bottomX` `bottomY` representan la coordenada inferior derecha del mismo.
+
+----------
 opencv-express
 ==============
 **This is an example of how to use [opencv4nodejs](https://github.com/justadudewhohacks/opencv4nodejs) in your web apps with express.js or any other serverside node.js framework. A minimalistic [docker image](https://hub.docker.com/r/justadudewhohacks/opencv-nodejs/) is provided with opencv-3.4.1 and opencv_contrib-3.4.1 + node.js environment set up (opencv4nodejs globally installed).**
--- src/services/lineDetection.js
+++ src/services/lineDetection.js
@@ -1,3 +1,5 @@
+const { decodeFromBase64, encodeJpgBase64 } = require('./imgcodecs');
+
 // Erosion -> Dilation constants
 const kernelED = new cv.Mat(5, 5, cv.CV_8UC1, 1);
 const anchorED = new cv.Point(-1, -1);
@@ -15,46 +17,46 @@ const beta = 1.0 - alpha;
 const CV_MORPH_RECT = 0;
 
 const boundingBox = contours => {
-	// Get bounding box corners
-	var minX = Number.POSITIVE_INFINITY;
-	var minY = Number.POSITIVE_INFINITY;
-	var maxX = Number.NEGATIVE_INFINITY;
-	var maxY = Number.NEGATIVE_INFINITY;
-	var i = 0;
-	contours.forEach(cnt => {
-		points = cnt.getPoints()
-		if (i > 0) {
-			cnt.getPoints().forEach(p => {
-				if (p.x < minX) {
-					minX = p.x;
-				}
-				if (p.y < minY) {
-					minY = p.y;
-				}
-				if (p.x > maxX) {
-					maxX = p.x;
-				}
-				if (p.y > maxY) {
-					maxY = p.y;
-				}
-			});
+    // Get bounding box corners
+    var minX = Number.POSITIVE_INFINITY;
+    var minY = Number.POSITIVE_INFINITY;
+    var maxX = Number.NEGATIVE_INFINITY;
+    var maxY = Number.NEGATIVE_INFINITY;
+    var i = 0;
+    contours.forEach(cnt => {
+	points = cnt.getPoints()
+	if (i > 0) {
+	    cnt.getPoints().forEach(p => {
+		if (p.x < minX) {
+		    minX = p.x;
 		}
-		i++;
-	});
-	return {
-		topCorner: new cv.Point(minX, minY),
-		bottomCorner: new cv.Point(maxX, maxY)
+		if (p.y < minY) {
+		    minY = p.y;
+		}
+		if (p.x > maxX) {
+		    maxX = p.x;
+		}
+		if (p.y > maxY) {
+		    maxY = p.y;
+		}
+	    });
 	}
+	i++;
+    });
+    return {
+	topCorner: new cv.Point(minX, minY),
+	bottomCorner: new cv.Point(maxX, maxY)
+    }
 };
 
 // Line computation (via kernels)
 const computeLines = img => {
-	// Defining a kernel length
+    // Defining a kernel length
     const kernelLength = Math.floor(img.cols / 80);
-	// A vertical kernel of (1 X kernel_length), which will detect all the vertical lines from the image.
-	const verticalKernel = cv.getStructuringElement(CV_MORPH_RECT, new cv.Size(1, kernelLength));
+    // A vertical kernel of (1 X kernel_length), which will detect all the vertical lines from the image.
+    const verticalKernel = cv.getStructuringElement(CV_MORPH_RECT, new cv.Size(1, kernelLength));
 
-	// A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
+    // A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
     const horiKernel = cv.getStructuringElement(CV_MORPH_RECT, new cv.Size(kernelLength, 1));
     // A kernel of (3 X 3) ones.
     const kernel = cv.getStructuringElement(CV_MORPH_RECT, new cv.Size(3, 3));
@@ -67,65 +69,107 @@ const computeLines = img => {
     const imgTemp2 = img.erode(horiKernel, anchorED, 3);
     const horizontalLinesImg = imgTemp2.dilate(horiKernel, anchorED, 3);
 
-	// Add two image with specific weight parameter to get a third image as summation of two image.
+    // Add two image with specific weight parameter to get a third image as summation of two image.
     const imgFinalBin = verticalLinesImg.addWeighted(alpha, horizontalLinesImg, beta, 0.0)
-		  .bitwiseNot().erode(kernel, anchorED, 2)
-		  .threshold(128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU);
+	  .bitwiseNot().erode(kernel, anchorED, 2)
+	  .threshold(128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU);
 
-	// Find contours for image, which will detect all the boxes
-	return imgFinalBin.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+    // Find contours for image, which will detect all the boxes
+    return imgFinalBin.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 };
 
 const edgeHighlight = img => {
-	// Grayscale image
-	const gray = img.bgrToGray();
+    // Grayscale image
+    const gray = img.bgrToGray();
 
-	// Blur and invert colours
-	const blur = gray.gaussianBlur(new cv.Size(3, 3), 0);
-	const inv = blur.bitwiseNot();
+    // Blur and invert colours
+    const blur = gray.gaussianBlur(new cv.Size(3, 3), 0);
+    const inv = blur.bitwiseNot();
 
-	// Erosion -> Dilation
-	const erosion = inv.erode(kernelED, anchorED, 1);
-	const dilation = erosion.dilate(kernelED, anchorED, 1);
+    // Erosion -> Dilation
+    const erosion = inv.erode(kernelED, anchorED, 1);
+    const dilation = erosion.dilate(kernelED, anchorED, 1);
 
-	// Canny edge detection
-	const blur2 = dilation.gaussianBlur(new cv.Size(3, 3), 0);
-	return blur2.canny(lowThresholdC, lowThresholdC*ratioC, kernelSizeC);
+    // Canny edge detection
+    const blur2 = dilation.gaussianBlur(new cv.Size(3, 3), 0);
+    return blur2.canny(lowThresholdC, lowThresholdC*ratioC, kernelSizeC);
 };
 
-const boxDoc = img => {
-	// Highlight edges
-	const edges = edgeHighlight(img);
+const boxDoc = (im, areaLoc, coverPercentage) => {
+    // Decode image
+    var img = decodeFromBase64(im);
+    const original = img.copy();
+    
+    // Parse area coordinates
+    const { topX, topY, bottomX, bottomY } = areaLoc;
 
-	// Compute horizontal and vertical lines
-	const lines = computeLines(edges);
-	console.log(lines.length);
+    // Crop area
+    width = bottomX - topX;
+    height = bottomY - topY;
+    area = img.getRegion(new cv.Rect(topX, topY, width, height));
+    
+    // Highlight edges
+    const edges = edgeHighlight(area);
 
-	// Compute bounding box
+    // Compute horizontal and vertical lines
+    const lines = computeLines(edges);
+    console.log(lines.length);
+
+    // Compute bounding box
+    const {topCorner, bottomCorner} = boundingBox(lines);
+
+    // Check if box is large enough
+    if (isFinite(topCorner.x) && isFinite(topCorner.y) && isFinite(bottomCorner.x) && isFinite(bottomCorner.y)) {
+	boxWidth = (bottomCorner.x - topCorner.x);
+	boxHeight = (bottomCorner.y - topCorner.y);
+	areaBox = boxWidth * boxHeight;
+	areaTotal = width * height;
+	idLoc = original.getRegion(new cv.Rect(topCorner.x, topCorner.y, boxWidth, boxHeight));
 	return {
-		corners: boundingBox(lines),
-		result: edges
+	    idImage: encodeJpgBase64(idLoc),
+	    decision: (areaBox / areaTotal) >= coverPercentage
 	};
+    } else {
+	return {
+	    idImage: null,
+	    decision: false
+	};
+    }
+};
+
+const boxDocAllImage = img => {
+    // Highlight edges
+    const edges = edgeHighlight(img);
+
+    // Compute horizontal and vertical lines
+    const lines = computeLines(edges);
+    console.log(lines.length);
+
+    // Compute bounding box
+    return {
+	corners: boundingBox(lines),
+	result: edges
+    };
 };
 
 module.exports = img => {
-	const original = img.copy();
+    const original = img.copy();
 
-	// Draw bounding box
-	const {corners, result} = boxDoc(img);
-	const {topCorner, bottomCorner} = corners;
-	if (isFinite(topCorner.x) && isFinite(topCorner.y) && isFinite(bottomCorner.x) && isFinite(bottomCorner.y)) {
-		console.log(topCorner);
-		console.log(bottomCorner);
-		original.drawRectangle(topCorner, bottomCorner, new cv.Vec(0, 253, 150), 5);
-		// Crop result
-		width = bottomCorner.x - topCorner.x;
-		height = bottomCorner.y - topCorner.y;
-		boxed = img.getRegion(new cv.Rect(topCorner.x, topCorner.y, width, height));
-		// return original;
-		return boxed;
-	} else {
-		console.log("Cannot find any important contour!");
-		return original;
-	}
+    // Draw bounding box
+    const {corners, result} = boxDocAllImage(img);
+    const {topCorner, bottomCorner} = corners;
+    if (isFinite(topCorner.x) && isFinite(topCorner.y) && isFinite(bottomCorner.x) && isFinite(bottomCorner.y)) {
+	console.log(topCorner);
+	console.log(bottomCorner);
+	original.drawRectangle(topCorner, bottomCorner, new cv.Vec(0, 253, 150), 5);
+	// Crop result
+	width = bottomCorner.x - topCorner.x;
+	height = bottomCorner.y - topCorner.y;
+	boxed = img.getRegion(new cv.Rect(topCorner.x, topCorner.y, width, height));
+	// return original;
+	return boxed;
+    } else {
+	console.log("Cannot find any important contour!");
+	return original;
+    }
 };
